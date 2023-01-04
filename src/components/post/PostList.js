2ed25d3d413f08ccac16e9ee0b26ef41b80aa5fd
jaedeokhan <hjaedeok@gmail.com>
2023-01-04 13:10:51 +0900

    Feat: PostList

--- src/components/post/PostList.js
+++ src/components/post/PostList.js
@@ -1,14 +1,13 @@
-import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import PersonIcon from '@mui/icons-material/Person';
 import IconButton from '@mui/material/IconButton';
-import userEvent from '@testing-library/user-event';
+import PinDropIcon from '@mui/icons-material/PinDrop';
+
+import styled from "styled-components";
 
 const PostList = () => {
 
@@ -21,13 +20,24 @@ const PostList = () => {
     }
 
     const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    
+    const HashTagStyleSpan = styled.span`
+      text-align : left;
+      font-size: 1.0rem;
+      padding: 0.25rem 0.7rem;
+      text-decoration: none;
+      letter-spacing: 0.10em;
+      background-color: #467fbf;
+      border-radius: 50px;
+      color: #fff;
+    `;
 
     return (
         <Grid container spacing={4}>
             {cards.map((card) => (
               <Grid item key={card} xs={12} sm={4} md={3}>
                 <Card
-                  sx={{ height: '100%', display: 'flex', flexDirection: 'column', backgroundColor : '#E2E2E2' }}
+                  sx={{ height: '100%', display: 'flex', flexDirection: 'column', backgroundColor :' rgb(249, 245, 245)' }}
                 >
                   <CardContent sx={{ flexGrow: 1 }}>
                     <IconButton
@@ -54,12 +64,13 @@ const PostList = () => {
                     alt="random"
                   />
                   <CardContent>
-                  <Typography align="left">
-                      {post.category}
-                  </Typography>
-                  <Typography align="right">
-                    {post.location}
-                  </Typography>
+                    <HashTagStyleSpan>
+                        {post.category}
+                    </HashTagStyleSpan>
+                    <Typography align="right">
+                        <PinDropIcon />
+                        {post.location}
+                    </Typography>
                   <Typography variant="h4">
                     {post.title}
                   </Typography>
--- src/pages/PostListPage.js
+++ src/pages/PostListPage.js
@@ -1,12 +1,31 @@
-import Button from '@mui/material/Button';
-import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 
 import PostList from '../components/post/PostList';
 
+import styled from "styled-components";
+
 const PostListPage = () => {
+
+  const MarginRightSpan = styled.span`
+    margin-right : 15px;
+    font-size: 1.3rem;
+  `;
+
+  const paramCity = "서울";
+  const categoryList = [
+    "사무용품",
+    "자전거",
+    "일상용품1",
+    "일상용품2",
+    "일상용품3",
+    "일상용품4",
+    "일상용품5",
+    "일상용품6",
+    "일상용품7",
+  ]
+
     return (
         <main>
         <Box
@@ -16,7 +35,7 @@ const PostListPage = () => {
             pb: 6,
           }}
         >
-          <Container maxWidth="sm">
+          <Container maxWidth="lg">
             <Typography
               component="h1"
               variant="h2"
@@ -24,22 +43,15 @@ const PostListPage = () => {
               color="text.primary"
               gutterBottom
             >
-              WSM 이다~
+              {paramCity}
+              <Typography align="center" color="text.secondary" paragraph>
+                {categoryList.map((category, index) => (
+                    <MarginRightSpan>
+                      #{category} 
+                    </MarginRightSpan>
+                      ))}
+              </Typography>
             </Typography>
-            <Typography variant="h5" align="center" color="text.secondary" paragraph>
-              Something short and leading about the collection below—its contents,
-              the creator, etc. Make it short and sweet, but not too short so folks
-              don&apos;t simply skip over it entirely.
-            </Typography>
-            <Stack
-              sx={{ pt: 4 }}
-              direction="row"
-              spacing={2}
-              justifyContent="center"
-            >
-              <Button variant="contained">Main call to action</Button>
-              <Button variant="outlined">Secondary action</Button>
-            </Stack>
           </Container>
         </Box>
         <Container sx={{ py: 8 }} maxWidth="lg">
